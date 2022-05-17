851ba1ba74c69a4bfaa79973145aaeeb8bc086de
Jahleel <Jahleelr-h@gmx.de>
2022-05-17 13:23:06 +0200

    set attacks & abilities

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ const allPokemons = [];
 
 let pokemonlimit = [151];
 
-
+//INIT FUNCTION ---> ONLOAD
 async function init() {
     await loadPokemons();
     renderPokedex();
 }
 
-
+//LOAD POKEMONS FROM API
 async function loadPokemons() {
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 151; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i + 1}`;
         let response = await fetch(url);
         currentPokemon = await response.json();
@@ -20,70 +20,49 @@ async function loadPokemons() {
     }
 }
 
-
+//RENDER & CREATE POKEDEX
 function renderPokedex() {
     let pokedex = document.getElementById('pokedex');
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 151; i++) {
         pokedex.innerHTML += renderPokemons(allPokemons, i);
     }
 }
-
 
 //OPEN BIG POKEMON-CARD
 function openDetailCard(id) {
     const foundPokemon = allPokemons.find(pokemon => pokemon['id'] === id);
     const i = allPokemons.findIndex(pokemon => pokemon['id'] === id);
-    console.log(foundPokemon);
-    console.log(i);
     let pokemonCards = document.getElementById('renderedPokemonCards');
-    renderPokemonAttacks(foundPokemon);
+
     pokemonCards.innerHTML = renderDetailCard(foundPokemon, i);
     pokemonCards.classList.remove('d-none');
     document.getElementById('pokedex').classList.add('d-none');
+
+    renderPokemonAttacks(foundPokemon);
+    renderPokemonAbilities(foundPokemon);
 }
-
-//CLOSE BIG POKEMON-CARD
-function quitDetailCard() {
-    document.getElementById('renderedPokemonCards').classList.add('d-none');
-    document.getElementById('pokedex').classList.remove('d-none');
-}
-
-
 
 
 function renderPokemonAttacks(foundPokemon) {
-
     let pokeAttackContainer = document.getElementById('pokemonAttacks');
+
     for (let i = 0; i < foundPokemon['abilities'].length; i++) {
-        pokeAttackContainer += foundPokemon['abilities'][i]['ability']['name'];
+        const abilitie = foundPokemon['abilities'][i]['ability']['name'];
+        pokeAttackContainer.innerHTML += /*html*/`
+        <div class="pokemon-attacks">${abilitie.replace(/^.{1}/g, abilitie[0].toUpperCase())}</div>`;
     }
-
-
-    //    document.getElementById('pokemonAttacks').innerHTML += currentPokemon['moves'][0]['move']['name'] += '\xa0\xa0';
-    //    document.getElementById('pokemonAttacks').innerHTML += currentPokemon['moves'][1]['move']['name'] += '\xa0\xa0';
-    //    document.getElementById('pokemonAttacks').innerHTML += currentPokemon['moves'][2]['move']['name'] += '\xa0\xa0';
-    //    document.getElementById('pokemonAttacks').innerHTML += currentPokemon['moves'][3]['move']['name'] += '\xa0\xa0';
-    //    document.getElementById('pokemonAttacks').innerHTML += currentPokemon['moves'][4]['move']['name'] += '\xa0\xa0';
 }
 
 
-function renderPokemonAbilities() {
 
-    let pokeAbilitiesContainer = document.getElementById('pokemonAttacks');
+function renderPokemonAbilities(foundPokemon) {
+    let pokeAbilitiesContainer = document.getElementById('pokemonAbilities');
+
     for (let i = 0; i < foundPokemon['moves'].length; i++) {
-        pokeAbilitiesContainer += foundPokemon['moves'][0]['move']['name'];
+        const move = foundPokemon['moves'][i]['move']['name'];
+        pokeAbilitiesContainer.innerHTML += /*html*/`
+        <div class="pokemon-abilities">${move.replace(/^.{1}/g, move[0].toUpperCase())}</div>`;
     }
-
-
-    //    document.getElementById('abilities').innerHTML = currentPokemon['abilities'][0]['ability']['name'] += '\xa0\xa0';
-    //    document.getElementById('abilities').innerHTML += currentPokemon['abilities'][1]['ability']['name'] += '\xa0\xa0';
-    //    document.getElementById('abilities').innerHTML = currentPokemon['abilities'][2]['ability']['name'];
-    //    document.getElementById('abilities').innerHTML = currentPokemon['abilities'][3]['ability']['name'];
-}
-
-
-function notIncluded() {
-    alert('not included yet');
 }
 
 //FIRST FUNCTION FOR THE POKECARDMENU
@@ -91,7 +70,7 @@ function changeOnStats() {
     document.getElementById('statsHeader').style.textDecoration = "underline";
     document.getElementById('attacksHeader').style.textDecoration = "none";
     document.getElementById('abilitiesHeader').style.textDecoration = "none";
-    document.getElementById('abilities').classList.add('d-none');
+    document.getElementById('pokemonAbilities').classList.add('d-none');
     document.getElementById('stats').classList.remove('d-none');
     document.getElementById('pokemonAttacks').classList.add('d-none');
 }
@@ -101,7 +80,7 @@ function changeOnAttacks() {
     document.getElementById('statsHeader').style.textDecoration = "none";
     document.getElementById('attacksHeader').style.textDecoration = "underline";
     document.getElementById('abilitiesHeader').style.textDecoration = "none";
-    document.getElementById('abilities').classList.add('d-none');
+    document.getElementById('pokemonAbilities').classList.add('d-none');
     document.getElementById('stats').classList.add('d-none');
     document.getElementById('pokemonAttacks').classList.remove('d-none');
 }
@@ -111,25 +90,36 @@ function changeOnAbilities() {
     document.getElementById('statsHeader').style.textDecoration = "none";
     document.getElementById('attacksHeader').style.textDecoration = "none";
     document.getElementById('abilitiesHeader').style.textDecoration = "underline";
-    document.getElementById('abilities').classList.remove('d-none');
+    document.getElementById('pokemonAbilities').classList.remove('d-none');
     document.getElementById('stats').classList.add('d-none');
     document.getElementById('pokemonAttacks').classList.add('d-none');
 }
 
+//CLOSE BIG POKEMON-CARD
+function quitDetailCard() {
+    document.getElementById('renderedPokemonCards').classList.add('d-none');
+    document.getElementById('pokedex').classList.remove('d-none');
+}
+
 //FUNCTION FOR SEARCHBAR
-function searchPokemon() {
+function searchPokemon(allPokemons) {
     value = document.getElementById('searchBar').value;
     pokemonExist = false;
-    eingabe = value.toLowerCase()
+    input = value.toLowerCase()
     for (let i = 0; i < allPokemons.length; i++) {
-        let element = allPokemons[i]['name']
-        if (eingabe == element) {
-            renderPokemonInfoCard(i);
+        let element = allPokemons[i]['name'];
+        if (input == element) {
             pokemonExist = true;
         }
+        renderPokedex();
     }
     if (pokemonExist == false) {
         alert('The Searched Pokemon is not avialable!');
         pokemonExist = false;
     }
+}
+
+//FUNCTION US NOT INCLUDED
+function notIncluded() {
+    alert('not included yet');
 }
--- js/template.js
+++ js/template.js
@@ -1,8 +1,8 @@
 function renderPokemons(allPokemons, i) {
     return `
     <div id="pokePreview_${i}" class="pokemon-preview color_bg_${allPokemons[i]['types'][0]['type']['name']}" onclick="openDetailCard(${allPokemons[i]['id']})">
-    <div id="pokenumber_${i}">#${i + 1}</div>
-    <h2 id="pokeName_${i}">${allPokemons[i]['name']}</h2>
+    <div id="pokenumber_${i}" class="poke-number">#${i + 1}</div>
+    <h2 id="pokeName_${i}" class="poke-name">${allPokemons[i]['name']}</h2>
     <img id="pokePicture_${i}" class="small-pokemons" src="${allPokemons[i]['sprites']['other']['official-artwork']['front_default']}">
     <div id="poketype_${i}" class="poketype">${allPokemons[i]['types'][0]['type']['name']}</div>
     </div>`
@@ -14,7 +14,7 @@ function renderDetailCard(foundPokemon, i) {
     <div id="pokemonCard_${i}" class="pokemon-info-card  color_bg_${foundPokemon['types'][0]['type']['name']}">
     <img class="arrow-back"  onclick="quitDetailCard(${i})" src="./img/arrow-121-32.png">
         <div id="pokecard">
-           <h2 id="pokemonName_${i}">${foundPokemon['name']}</h2>
+           <h2 id="pokemonName_${i}" class="poke-name">${foundPokemon['name']}</h2>
             <div class="pokemon-type-btn">
                 <span id="pokemonTypes_${i}">${foundPokemon['types'][0]['type']['name']}</span>
             </div>
@@ -27,17 +27,17 @@ function renderDetailCard(foundPokemon, i) {
                 <a id="abilitiesHeader" onclick="changeOnAbilities(${i})">Abilities</a>
             </nav>
 
-            <div id="pokemonAttacks" class="d-none">
-            ${foundPokemon['moves'][0]['move']['name']}
+            <div id="pokemonAttacks" class="attacks d-none">
+            
             </div>
 
-            <div id="abilities" class="abilities d-none">
-            ${foundPokemon['abilities'][0]['ability']['name']}
+            <div id="pokemonAbilities" class="abilities d-none">
+            
             </div>
 
             <div id="stats">
                 <div class="stats-class">
-                    <div id="hp">${foundPokemon['stats'][0]['stat']['name']}</div>
+                    <div id="hp" class="name">${foundPokemon['stats'][0]['stat']['name']}</div>
                     <div class="progress">
                         <div id="hpBar" class="progress-bar bg-success" role="progressbar" style="width: ${foundPokemon['stats'][0]['base_stat']}%"
                             aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${foundPokemon['stats'][0]['base_stat']}
@@ -45,7 +45,7 @@ function renderDetailCard(foundPokemon, i) {
                     </div>
                 </div>
                 <div class="stats-class">
-                    <div id="attack">${foundPokemon['stats'][1]['stat']['name']}</div>
+                    <div id="attack" class="name">${foundPokemon['stats'][1]['stat']['name']}</div>
                     <div class="progress">
                      <div id="attackBar" class="progress-bar bg-warning" role="progressbar" style="width: ${foundPokemon['stats'][1]['base_stat']}%"
                             aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${foundPokemon['stats'][1]['base_stat']}
@@ -53,7 +53,7 @@ function renderDetailCard(foundPokemon, i) {
                     </div>
                 </div>
                 <div class="stats-class">
-                    <div id="defense">${foundPokemon['stats'][2]['stat']['name']}</div>
+                    <div id="defense" class="name">${foundPokemon['stats'][2]['stat']['name']}</div>
                     <div class="progress">
                         <div id="defenseBar" class="progress-bar bg-danger" role="progressbar" style="width: ${foundPokemon['stats'][2]['base_stat']}%"
                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">${foundPokemon['stats'][2]['base_stat']}
@@ -61,7 +61,7 @@ function renderDetailCard(foundPokemon, i) {
                     </div>
                 </div>
                 <div class="stats-class">
-                    <div id="special-attack">${foundPokemon['stats'][3]['stat']['name']}</div>
+                    <div id="special-attack" class="name">${foundPokemon['stats'][3]['stat']['name']}</div>
                     <div class="progress">
                         <div id="saBar" class="progress-bar bg-warning" role="progressbar" style="width: ${foundPokemon['stats'][3]['base_stat']}%"
                             aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${foundPokemon['stats'][3]['base_stat']}
@@ -69,7 +69,7 @@ function renderDetailCard(foundPokemon, i) {
                     </div>
                 </div>
                     <div class="stats-class">
-                    <div id="special-defense">${foundPokemon['stats'][4]['stat']['name']}</div>
+                    <div id="special-defense" class="name">${foundPokemon['stats'][4]['stat']['name']}</div>
                     <div class="progress">
                         <div id="sdBar" class="progress-bar bg-danger" role="progressbar" style="width: ${foundPokemon['stats'][4]['base_stat']}%"
                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">${foundPokemon['stats'][4]['base_stat']}
@@ -77,7 +77,7 @@ function renderDetailCard(foundPokemon, i) {
                 </div>
                 </div>
                 <div class="stats-class">
-                    <div id="speed">${foundPokemon['stats'][5]['stat']['name']}</div>
+                    <div id="speed" class="name">${foundPokemon['stats'][5]['stat']['name']}</div>
                     <div class="progress">
                         <div id="speedBar" class="progress-bar bg-info" role="progressbar" style="width: ${foundPokemon['stats'][5]['base_stat']}%"
                             aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">${foundPokemon['stats'][5]['base_stat']}
